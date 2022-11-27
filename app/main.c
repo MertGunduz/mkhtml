9e42bcbaed85706af44b7bd4babf10edd008b86e
MertGunduz <merttgg@gmail.com>
2022-11-27 14:33:25 +0300

    mkhtml lexer command algo

--- app/main.c
+++ app/main.c
@@ -6,20 +6,25 @@
 #include "../src/genhtml/genhtml.h"
 #include "../src/genmessages/genmessages.h"
 
-#define HTML_GEN_ARGC 2
-#define HTMLCSS_GEN_ARGC 5
+#define ONE_COMMAND_ARGUMENT 2
+#define TWO_COMMAND_ARGUMENT 3
+#define FOUR_COMMAND_ARGUMENT 5
 
 int main(int argc, char *argv[])
 {
-    if (argc >= HTML_GEN_ARGC && argc <= HTMLCSS_GEN_ARGC) /* generation */
+    if (argc >= ONE_COMMAND_ARGUMENT && argc <= FOUR_COMMAND_ARGUMENT) /* generation */
     {
-        if (argc == HTML_GEN_ARGC) /* html generation */
+        if (argc == ONE_COMMAND_ARGUMENT) /* html generation */
         {
-            //
+            fprintf(stdout, "one command argument\n");
         }
-        else if (argc == HTMLCSS_GEN_ARGC) /* html-css generation */
+        else if (argc == TWO_COMMAND_ARGUMENT)
+        {
+            fprintf(stdout, "two command argument\n");
+        }
+        else if (argc == FOUR_COMMAND_ARGUMENT) /* html-css generation */
         {  
-            //
+            fprintf(stdout, "four command argument\n");
         }
     }
     else /* error */
