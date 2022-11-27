622873112105ecb9cca6fec71526ae93b9301db8
MertGunduz <merttgg@gmail.com>
2022-11-27 12:11:28 +0300

    gitignore file removed for the build dir

---
+++ build/src/genhtmlcss/CMakeFiles/genhtmlcss.dir/createHtmlCss.c.o.d
@@ -0,0 +1,4 @@
+src/genhtmlcss/CMakeFiles/genhtmlcss.dir/createHtmlCss.c.o: \
+ /home/mehmetmertgunduz/Projects/mkhtml/src/genhtmlcss/createHtmlCss.c \
+ /usr/include/stdc-predef.h \
+ /home/mehmetmertgunduz/Projects/mkhtml/src/genhtmlcss/genhtmlcss.h
---
+++ build/src/genhtmlcss/CMakeFiles/genhtmlcss.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for genhtmlcss.
+# This may be replaced when dependencies are built.
