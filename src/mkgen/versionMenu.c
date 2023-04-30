ebe794980c2ad2f52e9f1f429950f23422de1e4b
MertGunduz <merttgg@gmail.com>
2023-04-30 17:36:50 +0300

    version changed to 1.0.1

--- src/mkgen/versionMenu.c
+++ src/mkgen/versionMenu.c
@@ -13,7 +13,7 @@
 /// @brief prints the mkhtml version
 void versionMenu()
 {
-    fprintf(stdout, "mkhtml version 1.0.0\n\n");
+    fprintf(stdout, "mkhtml version 1.0.1\n\n");
     fprintf(stdout, "license GPL 3.0: GNU GPL version 3 https://gnu.org/licenses/gpl.html\n");
     fprintf(stdout, "this is a free software: you are free to change and redistribute it.\n");
     fprintf(stdout, "there is no warranty, to the extent permitted by law.\n\n");
