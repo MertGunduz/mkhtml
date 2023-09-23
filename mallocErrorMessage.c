a1f9145d73e389cac0605e8d420011a3d07b16f0
Furokop <145336663+Furokop@users.noreply.github.com>
2023-09-23 09:51:14 +0000

    Delete mallocErrorMessage.c

--- mallocErrorMessage.c
+++
@@ -1,7 +0,0 @@
-
-#include "mkgenmessages.h"
-
-void mallocErrorMessage() {
-    fprintf(stderr, "mkhtml: can\'t allocate memory for the processes.\n");
-    fprintf(stderr, "mkhtml: you might be out of swap space or memory, please check memory.\n");
-}
